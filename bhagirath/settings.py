7daf284df9271eae22d39b65fbb051d59ea6ccea
Venkata Pingali <pingali@gmail.com>
2012-04-04 05:36:59 +0000

    Fixed the module path for auth context processor which has moved to contrib

--- bhagirath/settings.py
+++ bhagirath/settings.py
@@ -176,7 +176,7 @@ TEMPLATE_DIRS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     # default template context processors
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
